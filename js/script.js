94518c9073d92216faa1520d9924b6f5eff2e121
JKauffman27 <jkauffman@hawk.iit.edu>
2023-11-07 18:44:33 -0500

    making the required changes

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 function init(){
 //add your javascrip between these two lines of code
- 
-
-
-
-
-
-
+  var button = document.getElementByID('entrybutton');
+  
+  function showText() {
+	var txt_box = document.getElementByID('entryinput');
+	document.getElementByID('textoutput').innerHTML = txt_box.value
+	alert("Jordan Kauffman: " + txt_box.value);
+  }
+  
+  button.addEventListener('click', showText);
+  }
 
 window.addEventListener('load', init);

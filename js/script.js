df930aaf737b265b506b66095d47a41b8407f93e
JKauffman27 <jkauffman@hawk.iit.edu>
2023-11-07 18:56:52 -0500

    Changing the lab number and fixing a mistake (incorrect function)

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 function init(){
 //add your javascrip between these two lines of code
-  var button = document.getElementByID('entrybutton');
+  var button = document.getElementById('entrybutton');
   
   function showText() {
-	var txt_box = document.getElementByID('entryinput');
-	document.getElementByID('textoutput').innerHTML = txt_box.value
+	var txt_box = document.getElementById('entryinput');
+	document.getElementById('textoutput').innerHTML = txt_box.value
 	alert("Jordan Kauffman: " + txt_box.value);
   }
   
